a4bdc6a0398208e4981c7c29f8a43881bf223d1e
cici0828 <dde20011@hotmail.com>
2018-04-15 22:12:39 +0800

    update1

--- src/main/java/HisCommunication.java
+++ src/main/java/HisCommunication.java
@@ -1,3 +1,33 @@
 public class HisCommunication {
-    //
+
+    public static void main(String[] args) {
+        final Bussiness bussiness = new Bussiness();
+
+        new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 50; i++) {
+                    bussiness.sub(i);
+                }
+
+            }
+        }).start();
+
+        for (int i = 0; i < 50; i++) {
+            bussiness.main(i);
+        }
+    }
+
+    private static class Bussiness{
+        public synchronized void  sub(int j){
+            for (int i = 0; i < 10 ; i++) {
+                System.out.println("sub:" + i + " of " + j);
+            }
+        }
+
+        public  synchronized void main(int j){
+            for (int i = 0; i<100; i++){
+                System.out.println("main:" + i + " of " + j);
+            }
+        }
+    }
 }
